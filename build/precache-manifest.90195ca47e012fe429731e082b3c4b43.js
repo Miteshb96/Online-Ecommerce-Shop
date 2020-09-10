be47b8e105b5dc9f04a3aade6ecddb89e0c1ce2f
Mitesh Bhanushali <miteshbhanushali@livquik.com>
2020-09-11 00:44:50 +0530

    build folder added

---
+++ build/precache-manifest.90195ca47e012fe429731e082b3c4b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d10dbf81fe4c81b6b5ab0d93266247a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb82ae3fc68e6e452c8",
+    "url": "/static/css/main.30725782.chunk.css"
+  },
+  {
+    "revision": "c228885543545666650c",
+    "url": "/static/js/2.eec376a2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eec376a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb82ae3fc68e6e452c8",
+    "url": "/static/js/main.cfc4cb94.chunk.js"
+  },
+  {
+    "revision": "17b6fb54f92081bbfe90",
+    "url": "/static/js/runtime-main.8dd6416e.js"
+  }
+]);
