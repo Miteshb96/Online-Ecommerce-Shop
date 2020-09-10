a80635e4be56563741457f2f9840ff4f166d2f65
Mitesh Bhanushali <miteshbhanushali@livquik.com>
2020-09-11 00:43:40 +0530

    deployment config changes updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require('/public');
+const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '.', 'public');
+const port = process.env.PORT || 3001;
+const publicPath = path.join(__dirname, 'build');
 
-app.use(express.static(publicPath, 'index.html'));
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
